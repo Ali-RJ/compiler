7e6c17252b621de38074499e34ba5213fb805acb
ali <63716594+alexrajabi@users.noreply.github.com>
2024-09-12 00:20:58 +0330

    init

---
+++ kfc.py
@@ -0,0 +1,22 @@
+from lex import *
+from parse import *
+from emit import *
+import sys
+
+def main():
+    print("K For Compiler")
+
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    with open(sys.argv[1], 'r') as inputFile:
+        source = inputFile.read()
+
+    lexer = Lexer(source)
+    emitter = Emitter("out.c")
+    parser = Parser(lexer, emitter)
+
+    parser.program()
+    emitter.writeFile()
+    print("Compiling completed!")
+
+main()
